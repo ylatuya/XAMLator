67850f2bdf68e7e1fd66f7a881d383eacaabffbb
Andoni Morales Alastruey <ylatuya@gmail.com>
2018-09-26 19:17:02 +0200

    Bump addin version

--- XAMLator.Client.MonoDevelop/Properties/AddinInfo.cs
+++ XAMLator.Client.MonoDevelop/Properties/AddinInfo.cs
@@ -30,7 +30,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
 	"XAMLator.Client.MonoDevelop",
 	Namespace = "XAMLator.Client.MonoDevelop",
-	Version = "1.0.1"
+	Version = "1.0.2"
 )]
 
 [assembly: AddinName("XAMLator.Client.MonoDevelop")]
