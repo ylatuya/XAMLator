2ee6a1d8edd6d30d34598ea43136466df215d8d7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-10-17 20:09:56 +0200

    Added XAMLator.SampleApp.MacOS

---
+++ SampleApp/XAMLator.SampleApp.MacOS/AppDelegate.cs
@@ -0,0 +1,42 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace XAMLator.SampleApp.MacOS
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : FormsApplicationDelegate
+	{
+		NSWindow _window;
+
+		public AppDelegate()
+		{
+			var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+			var rect = new CoreGraphics.CGRect(500, 1000, 500, 500);
+			_window = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+			{
+				Title = "XAMLator Sample App",
+				TitleVisibility = NSWindowTitleVisibility.Hidden
+			};
+		}
+
+		public override NSWindow MainWindow
+		{
+			get { return _window; }
+		}
+
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			Forms.Init();
+			LoadApplication(new App());
+			base.DidFinishLaunching(notification);
+		}
+
+		public override void WillTerminate(NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+	}
+}
