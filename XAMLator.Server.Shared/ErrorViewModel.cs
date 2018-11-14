a229081f7e76feb56dab042bddcc6b2b03e3229d
Andoni Morales Alastruey <ylatuya@gmail.com>
2018-11-14 19:53:43 +0100

    Fix close command in errors reporting

--- XAMLator.Server.Shared/ErrorViewModel.cs
+++ XAMLator.Server.Shared/ErrorViewModel.cs
@@ -1,41 +1,71 @@
 ﻿//
 //  Copyright (C) 2018 Fluendo S.A.
 using System;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace XAMLator.Server
 {
 	public class ErrorViewModel : INotifyPropertyChanged
 	{
+		private string _title;
+		private string _error;
+		private ICommand _closeCommand;
+
 		public event PropertyChangedEventHandler PropertyChanged;
 
-		string title;
-		string error;
-
-		public ErrorViewModel (string title, Exception ex)
+		public string Title
 		{
-			this.error = ex.ToString ();
-			this.title = title;
+			get => _title;
+			set
+			{
+				_title = value;
+				EmitPropertyChanged(nameof(Title));
+			}
 		}
 
-		public ErrorViewModel (string title, EvalResult result)
+		public string Error
 		{
-			error = result.Messages [0].Text;
-			this.title = title;
+			get => _error;
+			set
+			{
+				_error = value;
+				EmitPropertyChanged(nameof(Error));
+			}
 		}
 
-		public string Title {
-			get => title;
-		}
-
-		public string Error {
-			get => error;
-		}
-
-		void EmitPropertyChanged (string v)
+		public ICommand CloseCommand
 		{
-			PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (v));
+			get => _closeCommand;
+			set
+			{
+				_closeCommand = value;
+				EmitPropertyChanged(nameof(CloseCommand));
+			}
+		}
+
+		public void SetError(string title, Exception ex)
+		{
+			Error = ex.ToString();
+			Title = title;
+		}
+
+		public void SetError(string title, EvalResult result)
+		{
+			if (result.Messages.Length > 0)
+			{
+				Error = result.Messages[0].Text;
+			}
+			else
+			{
+				Error = "Unknown error";
+			}
+			Title = title;
+		}
+
+		void EmitPropertyChanged(string v)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
 		}
 	}
 }
