373fddd81fb32facdf1c28893506289d6f31f233
Andoni Morales Alastruey <ylatuya@gmail.com>
2019-02-22 21:31:37 +0100

    Use a different port for the server in DEBUG mode

--- XAMLator.Core/Constants.cs
+++ XAMLator.Core/Constants.cs
@@ -9,7 +9,11 @@ namespace XAMLator
 	{
 		public const string DEFAULT_HOST = "127.0.0.1";
 
+#if DEBUG
+		public const int DEFAULT_PORT = 9988;
+#else
 		public const int DEFAULT_PORT = 8488;
+#endif
 
 		public const string XAMLATOR_ASSEMBLY = "XAMLator.Server.dll";
 
